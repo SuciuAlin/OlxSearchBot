57916a24b6781ccc0769a2adc5a95df388f5a0ba
Alin Suciu <suciualinflorin@gmail.com>
2020-01-01 19:30:56 +0200

    app with manual config

--- OlxBot.mm
+++ OlxBot.mm
@@ -1,26 +1,30 @@
 <map version="1.0.1">
 <!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
-<node CREATED="1577472166275" ID="ID_192904215" MODIFIED="1577472890359" STYLE="fork" TEXT="OlxBot">
-<node CREATED="1577472264941" ID="ID_549140012" MODIFIED="1577472838052" POSITION="right" TEXT="connect to olx/electronics">
+<node CREATED="1577472166275" ID="ID_192904215" MODIFIED="1577898084910" STYLE="fork" TEXT="OlxBot">
+<node CREATED="1577898096714" FOLDED="true" ID="ID_1774970909" MODIFIED="1577898835398" POSITION="right" TEXT="skelet ideas">
+<icon BUILTIN="flag-green"/>
+<node CREATED="1577472264941" ID="ID_549140012" MODIFIED="1577472838052" TEXT="connect to olx/electronics">
 <icon BUILTIN="flag-green"/>
 </node>
-<node CREATED="1577472281358" ID="ID_832242380" MODIFIED="1577472842026" POSITION="right" TEXT="add price range">
+<node CREATED="1577472281358" ID="ID_832242380" MODIFIED="1577472842026" TEXT="add price range">
 <icon BUILTIN="flag-green"/>
 </node>
-<node CREATED="1577472352812" ID="ID_1946627312" MODIFIED="1577472844496" POSITION="right" TEXT="order is by newest date by default">
+<node CREATED="1577472352812" ID="ID_1946627312" MODIFIED="1577472844496" TEXT="order is by newest date by default">
 <icon BUILTIN="flag-green"/>
 </node>
-<node CREATED="1577472366238" ID="ID_1934015086" MODIFIED="1577472854217" POSITION="right" TEXT="get elements attributes by xpath">
+<node CREATED="1577472366238" ID="ID_1934015086" MODIFIED="1577472854217" TEXT="get elements attributes by xpath">
 <icon BUILTIN="flag-green"/>
 </node>
-<node CREATED="1577472398011" FOLDED="true" ID="ID_1519753546" MODIFIED="1577473013831" POSITION="right" TEXT="write elements to new html page">
+<node CREATED="1577472398011" FOLDED="true" ID="ID_1519753546" MODIFIED="1577898114519" TEXT="write elements to new html page">
 <icon BUILTIN="flag-green"/>
 <node CREATED="1577472415967" ID="ID_1518696811" MODIFIED="1577472420255" TEXT="price"/>
 <node CREATED="1577472420877" ID="ID_541345400" MODIFIED="1577472440450" TEXT="location"/>
 <node CREATED="1577472441009" ID="ID_356878360" MODIFIED="1577472453283" TEXT="posted date"/>
 </node>
+</node>
+<node CREATED="1577898129664" ID="ID_559395420" MODIFIED="1577898163379" POSITION="right" TEXT="importance of app: notify about new items that the user searched for"/>
 <node CREATED="1577472411524" ID="ID_226063219" MODIFIED="1577472479253" POSITION="right" TEXT="notify when new item available">
-<node CREATED="1577472482929" ID="ID_804197403" MODIFIED="1577472757507" TEXT="create a temp file">
+<node CREATED="1577472482929" ID="ID_804197403" MODIFIED="1577898054036" TEXT="create a temp file(storage.txt)">
 <node CREATED="1577472529504" ID="ID_991505284" MODIFIED="1577473007984" TEXT="if it&apos;s the same with the existing one, do nothing">
 <icon BUILTIN="button_ok"/>
 </node>
@@ -29,6 +33,12 @@
 <node CREATED="1577472640364" ID="ID_1716537613" MODIFIED="1577472662443" TEXT="IMPORTANT android"/>
 </node>
 </node>
+</node>
+<node CREATED="1577818874891" ID="ID_559932962" MODIFIED="1577898076041" POSITION="left" TEXT="run in backround without opening new files">
+<node CREATED="1577898177837" ID="ID_1373616609" MODIFIED="1577898182654" TEXT="headless chrome"/>
+</node>
+<node CREATED="1577898247701" ID="ID_537899200" MODIFIED="1577898837149" POSITION="left" TEXT="webpage with the newest page on that search">
+<icon BUILTIN="flag-green"/>
 </node>
 </node>
 </map>
--- main.py
+++ main.py
@@ -3,83 +3,234 @@ from time import sleep
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.common.exceptions import NoSuchElementException
 
 
 class InstaBot:
-    def __init__(self,site,filename):
+    def __init__(self, filename, storagefile, config):
         self.driver = webdriver.Chrome()
         # e nevoie de pret minim si pret maxim pentru a sorta dupa
-        self.driver.get("https://www.olx.ro/electronice-si-electrocasnice/?search%5Bfilter_float_price%3Afrom%5D=2&search%5Bfilter_float_price%3Ato%5D=1")
+        self.driver.get("https://www.olx.ro/"
+                        + config["category"]
+                        + "/?search%5Bfilter_float_price%3Afrom%5D=2&search%5Bfilter_float_price%3Ato%5D=1&currency="
+                        + config["currency"])
         self.__filename = filename
-
-    def query(self):
-        self.driver.find_element_by_xpath("//input[@name='q']")\
-            .send_keys("dell xps 13")
-        self.driver.find_element_by_xpath("//input[@id='submit-searchmain']")\
-            .click()
+        self.__searchedItem = config["searchedItem"]
+        self.__priceFrom = config["priceFrom"]
+        self.__priceTo = config["priceTo"]
+        self.__storagefile = storagefile
 
     def laptopQuery(self):
-        
-        self.driver.find_element_by_xpath("//input[@id='search-text']")\
-            .send_keys("dell xps 13 i7 512")
-        sleep(2)
 
-        minimValueInput = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@name="search[filter_float_price:from]"]')))
-        self.driver.execute_script("arguments[0].value = '1900';", minimValueInput)
+        self.driver.find_element_by_xpath("//input[@id='search-text']") \
+            .send_keys(self.__searchedItem)
+        sleep(1)
 
-        maximValueInput = WebDriverWait(self.driver, 20).until(EC.presence_of_element_located((By.XPATH, '//input[@name="search[filter_float_price:to]"]')))
-        self.driver.execute_script("arguments[0].value = '5200';", maximValueInput)
+        minimValueInput = WebDriverWait(self.driver, 20) \
+            .until(EC.presence_of_element_located \
+                       ((By.XPATH, '//input[@name="search[filter_float_price:from]"]')))
 
-        self.driver.find_element_by_xpath("//input[@id='search-submit']")\
+        self.driver.execute_script("arguments[0].value = " + self.__priceFrom + ";", minimValueInput)
+
+        maximValueInput = WebDriverWait(self.driver, 20) \
+            .until(EC.presence_of_element_located \
+                       ((By.XPATH, '//input[@name="search[filter_float_price:to]"]')))
+
+        self.driver.execute_script("arguments[0].value = " + self.__priceTo + ";", maximValueInput)
+
+        self.driver.find_element_by_xpath("//input[@id='search-submit']") \
             .click()
 
-    def returnPriceElement(self,i):
+    def __createListWithResultedTr(self,n):
+        list = []
+        missing = 0
+        i = 0
+        while i < n + missing:
+            try:
+                list.append(self.__returnPriceElement(i))
+                list[-1] = i
+            except:
+                missing += 1
+            i += 1
+        return list
+
+    def __returnPriceElement(self, i):
+        return self.driver.find_element_by_xpath(
+            '// *[ @ id = "offers_table"] / tbody / tr['
+            + str(i)
+            + '] / td / div / table / tbody / tr[1] / td[3] / div / p / strong')
+
+    def __returnLocationElement(self, i):
+        return self.driver.find_element_by_xpath(
+            '//*[@id="offers_table"]/tbody/tr['
+            + str(i)
+            + ']/td/div/table/tbody/tr[2]/td[1]/div/p/small[1]/span')
+
+    def __returnDateElement(self, i):
+        return self.driver.find_element_by_xpath(
+            '//*[@id="offers_table"]/tbody/tr['
+            + str(i)
+            + ']/td/div/table/tbody/tr[2]/td[1]/div/p/small[2]/span')
+
+    def __returnPriceElementList(self, n):
         '''
-        i+4 - asa pleaca in pagina de la row 4
+        there are missing random columns in results for randomness
+        but always starts with 4
         '''
-        return self.driver.find_element_by_xpath('// *[ @ id = "offers_table"] / tbody / tr['+str(i)+'] / td / div / table / tbody / tr[1] / td[3] / div / p / strong')
+        #print(n)
+        list = self.__createListWithResultedTr(n)
+        elementList = []
+        for i in range(0, len(list)):
+            elementList.append(self.__returnPriceElement(list[i]))
 
-    def returnHrefElement(self,i):
-        return self.driver.find_element_by_xpath('//*[@id="offers_table"]/tbody/tr['+str(i)+']/td/div/table/tbody/tr[1]/td[2]/div/h3/a')
+        return elementList
 
-    def returnLocationElement(self,i):
-        return self.driver.find_element_by_xpath('//*[@id="offers_table"]/tbody/tr['+str(i)+']/td/div/table/tbody/tr[2]/td[1]/div/p/small[1]/span')
-
-    def returnDateElement(self,i):
-        return self.driver.find_element_by_xpath('//*[@id="offers_table"]/tbody/tr['+str(i)+']/td/div/table/tbody/tr[2]/td[1]/div/p/small[2]/span')
-
-    def returnHrefElementList(self):
+    def __returnHrefElementList(self):
         elems_href = self.driver.find_elements_by_xpath("//a[contains(@href,'oferta')]")
         for i in range(0, len(elems_href)):
             elems_href[i] = elems_href[i].get_attribute("href").split('#')[0]
         return list(dict.fromkeys(elems_href))
 
-    def createWebpage(self):
-        sleep(4)
-        elems_href = self.returnHrefElementList()
+    def __returnHrefElement(self, i):
+        return self.driver.find_element_by_xpath(
+            '//*[@id="offers_table"]/tbody/tr['
+            + str(i)
+            + ']/td/div/table/tbody/tr[1]/td[2]/div/h3/a')
 
+    def __priceFormatToInt(self, priceFormat):
+        priceCuts = priceFormat.text.split(" ")
+        sum = 0
+        for i in range(0, len(priceCuts) - 1):
+            sum = sum * 1000 + int(priceCuts[i])
+        return sum
 
-        file = open(self.__filename,'w')
+    def __verifyTheExistenceOfItems(self, n):
+        '''
+        verify if objects with bigger/smaller values than ones sets appear
+        gets error cause it will see more values than needed
+        because of the random ones generated by the page
+        and will try to access an inexisting ones
+        '''
+        pricesList = self.__returnPriceElementList(n)
+        for i in range(0, n):
+            sum = self.__priceFormatToInt(pricesList[i])
+            if sum > int(self.__priceTo) or sum < int(self.__priceFrom):
+                return False
+        return True
 
+    def __writeTheItemsInFile(self, elemsHref,trUsed):
+        file = open(self.__filename, 'w')
+        numberOfItems = len(elemsHref)
         file.write("<html><head><title>OlxOffers</title></head><body><table border='yes'>")
-        for i in range(4, 4+len(elems_href)):
-            file.write("<tr><td>")
-            file.write(self.returnPriceElement(i).text)
-            file.write("</td><td>")
-            file.write(self.returnLocationElement(i).text)
-            file.write("</td><td>")
-            file.write(self.returnDateElement(i).text)
-            file.write("</td><td><a href="+elems_href[i-4]+">")
-            file.write(elems_href[i-4])
-            file.write("</a></td></tr>")
+        storagefile = open(self.__storagefile, 'w')
+        for i in range(0, min(3,numberOfItems)):
+            storagefile.write(self.__returnHrefElement(trUsed[i]).get_attribute("href").split('#')[0])
+            storagefile.write('\n')
+        storagefile.close()
+        for i in range(0, numberOfItems):
+            file.write("<tr>\n\n<td>")
+            file.write(self.__returnPriceElement(trUsed[i]).text)
+            file.write("</td>\n<td>")
+            file.write(self.__returnLocationElement(trUsed[i]).text)
+            file.write("</td>\n<td>")
+            file.write(self.__returnDateElement(trUsed[i]).text)
+            file.write("</td>\n<td><a href=" + self.__returnHrefElement(trUsed[i]).get_attribute("href") .split('#')[0]+ ">\n")
+            file.write(self.__returnHrefElement(trUsed[i]).text)
+            file.write("\n</a></td></tr>")
+        file.write("\n</table></html>")
+        file.close()
+
+    '''deprecated
+    def __writeTheLastThreeOrLessItemsInFile(self,elemsHref):
+        f = open(self.__storagefile,'w')
+        for i in range(0,min(3,len(elemsHref))):
+            f.write(elemsHref[i])
+            f.write('\n')
+        f.close()
+    '''
+
+    def __readTheLastThreeAndReturnList(self):
+        f = open(self.__storagefile, 'r')
+        lines = f.readlines()
+        f.close()
+        return lines
+
+    def __returnTrueIfNewItemsAppear(self,elemsHref,trUsed,numberOfItems):
+        lines = self.__readTheLastThreeAndReturnList()
+
+
+        #daca sunt la fel de multe se verifica conditia de aparitie
+        n = min(numberOfItems, len(elemsHref))
+        newItems = []
+        oldItems = []
+        for i in range(0, n):
+            newItems.append(self.__returnHrefElement(trUsed[i]).get_attribute("href").split('#')[0])
+            oldItems.append(lines[i])
+
+        #ok - True if new items appears, False otherwise
+        ok = True
+        for i in range(0, n):
+            if newItems[0].strip() == oldItems[i].strip():
+                ok = False
+
+        return ok
+
+    def __writeNoItemsInFile(self):
+        file = open(self.__filename, 'w')
+        file.write("<tr><td>")
+        file.write("Nu sunt elemente!")  # no elements found
+        file.write("</td></tr>")
         file.write("</table></html>")
+        file.close()
 
+    def createWebpage(self):
+        sleep(3)
+        elemsHref = self.__returnHrefElementList()
+        numberOfItems = len(elemsHref)
+        trUsed = self.__createListWithResultedTr(numberOfItems)
 
+        try:
+            self.__verifyTheExistenceOfItems(numberOfItems)
+            if self.__returnTrueIfNewItemsAppear(elemsHref, trUsed, 3):
 
-    def iesi(self):
+                print("e diferita lista")
+                self.__writeTheItemsInFile(elemsHref, trUsed)
+            else:
+                print("e identica lista")
+        except NoSuchElementException:
+            self.__writeNoItemsInFile()
+
+    def quit(self):
         self.driver.close()
 
-olxBot = InstaBot("olx.ro","olxTable.html")
+
+def getValueThatContainsXFromList(list, dict, x):
+    for line in list:
+        if x in line:
+            dict[x] = line.strip().split('=')[1].strip()
+
+def dataFromConfig():
+    f = open("config.txt", "r")
+    lines = f.readlines()
+    dictionary = {}
+    dictionary["category"] = "none"
+    dictionary["searchedItem"] = "none"
+    dictionary["priceFrom"] = "none"
+    dictionary["priceTo"] = "none"
+
+    getValueThatContainsXFromList(lines, dictionary, "category")
+    getValueThatContainsXFromList(lines, dictionary, "searchedItem")
+    getValueThatContainsXFromList(lines, dictionary, "priceFrom")
+    getValueThatContainsXFromList(lines, dictionary, "priceTo")
+    getValueThatContainsXFromList(lines, dictionary, "currency")
+    f.close()
+
+    return dictionary
+
+storagefile = "storage.txt"
+fileName = "olxSearched.html"
+config = dataFromConfig()
+olxBot = InstaBot(fileName, storagefile, config)
 olxBot.laptopQuery()
 olxBot.createWebpage()
-olxBot.iesi()
+olxBot.quit()
